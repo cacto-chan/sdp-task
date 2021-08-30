c9ba565b7c9287135a3128642bc21827cf6ba0a7
cacto-chan <89793519+cacto-chan@users.noreply.github.com>
2021-08-30 22:43:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# WAP for add,mul,div,sub
+# for addition
+a = 23
+b = 5
+print(a+b)
+
+# for subtraction
+a = 23
+b = 5
+print(a-b)
+
+# for multiplication
+a = 23
+b = 5
+print(a*b)
+
+# for division
+a = 23
+b = 5
+print(a/b)
